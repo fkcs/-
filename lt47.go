e2961dbc7093e84eb2631fae145aa09f8839e36b
fkcs <71970462+fkcs@users.noreply.github.com>
2020-10-01 12:13:54 +0800

    Create lt47.go
    力扣47，全排列II

---
+++ lt47.go
@@ -0,0 +1,31 @@
+func recurseSum2(candidates []int, ret *[][]int, path *[]int, target, index int) {
+	if target != 0 && index == len(candidates) {
+		return
+	}
+	if target == 0 {
+		tmp := make([]int,len(*path))
+		copy(tmp,*path)
+		*ret = append(*ret,tmp)
+		return
+	}
+
+	for i:=index; i<len(candidates); i++ {
+		if target-candidates[i] < 0 {
+			continue
+		}
+		*path = append(*path,candidates[i])
+		recurseSum2(candidates,ret,path,target-candidates[i],i+1)
+		*path = (*path)[:len(*path)-1]
+		for i<len(candidates)-1 && candidates[i] == candidates[i+1] {
+			i++
+		}
+	}
+}
+
+func combinationSum2(candidates []int, target int) [][]int {
+	sort.Ints(candidates)
+	ret := make([][]int,0)
+	path := make([]int,0)
+	recurseSum2(candidates,&ret,&path,target,0)
+	return ret
+}
